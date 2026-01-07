8ab6eb334a68a0c66cc20898986c3467af966237
Arun Mishra <arunnmishra@gmail.com>
2026-01-07 22:01:20 +0530

    Week#1D4_1

--- 01_cpp_foundation/benchmarks/copy_vs_move.cpp
+++ 01_cpp_foundation/benchmarks/copy_vs_move.cpp
@@ -35,5 +35,17 @@ public:
 int main(void){
     echo_data *data1 = new echo_data();
     echo_data *data2 =data1; //here constructor is not called will see where all copy constructor is called
+    //Where all copy called 
+    /*
+    Most surprising / dangerous copies
+   ↓
+    5. catch (Type e) by value
+    4. throw expr;  (copy of thrown object)
+    3. Parameter of type std::function<T>
+    2. Returning big object from getter (T getX() const)
+    1. Adding many elements to vector → reallocation
+    ↓
+    Least surprising
+    */ 
     return 0;
 }
