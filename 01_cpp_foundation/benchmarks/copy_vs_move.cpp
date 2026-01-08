66a616c03ec4d2bcb9f6e7188f26155cd2b466ba
Arun Mishra <arunnmishra@gmail.com>
2026-01-08 22:03:18 +0530

    Week#1D5_1

--- 01_cpp_foundation/benchmarks/copy_vs_move.cpp
+++ 01_cpp_foundation/benchmarks/copy_vs_move.cpp
@@ -15,10 +15,17 @@ public:
     coordinates(){
         cout<<"construtor coordinates"<<endl;
     }
+    coordinates(int a, int b, int c): x1(a),x2{b}, x3{c}{
+        cout<<"3 param construtor coordinates"<<endl;
+    }
+    coordinates(const coordinates &obj): x1{obj.x1}, x2{obj.x2}, x3{obj.x3} {
+        cout<<"copy construtor coordinates"<<endl;
+    }
 };
 
 class echo_data{
-    vector<coordinates> d{10};
+    vector<coordinates> d{10}; //this will call 10x constructor 
+    coordinates c{2,4,6}; //this will call a 3 parma constructor 
     char *dp = "data_point1";
 public:
     string get_dp_name(){
@@ -28,6 +35,12 @@ public:
     echo_data(){
         cout<<"construtor echo_data"<<endl;
     }
+
+    //default constructor 
+    echo_data(const echo_data & obj): d{obj.d}, c{obj.c}, dp{obj.dp} {
+        cout<<"copy construtor echo_data"<<endl;
+    }
+
     //default assignment 
 };
 
@@ -35,6 +48,12 @@ public:
 int main(void){
     echo_data *data1 = new echo_data();
     echo_data *data2 =data1; //here constructor is not called will see where all copy constructor is called
+
+    echo_data d1;
+    cout<<"****************"<<endl;
+    echo_data d2{d1}; //copy construtor shall call. not clear if copy constructor of echo_data is not defined , 
+                      //then coordinates copy constructor is called , if deined then normal constructor is called ?
+
     //Where all copy called 
     /*
     Most surprising / dangerous copies
