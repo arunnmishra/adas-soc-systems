7138156ca915a2c331ff9df21775a0d654208105
Arun Mishra <arunnmishra@gmail.com>
2026-01-12 22:36:50 +0530

    Week2D1_1

--- 01_cpp_foundation/benchmarks/copy_vs_move.cpp
+++ 01_cpp_foundation/benchmarks/copy_vs_move.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class BigBuffer{
     vector<int> buff;
     string buff_name;
-    char *pImpl;            //next make it ==> std::unique_ptr
+    char *pImpl;            //next make it ==> std::unique_ptr Then re-observe: copy becomes deleted 2- move becomes automatic
 public:
     //constructor
     BigBuffer(size_t n=1000000, string s="default buffer"):buff(n), buff_name(s){
@@ -38,10 +38,10 @@ public:
     }
 
     //move constructor, to eliminate the copy penality wherever psossible to transfer ownership
-    BigBuffer(BigBuffer &&r_value):buff(std::move(r_value.buff)),buff_name(std::move(r_value.buff_name)),
+    BigBuffer(BigBuffer &&r_value) noexcept :buff(std::move(r_value.buff)),buff_name(std::move(r_value.buff_name)),
                                     pImpl(r_value.pImpl){
         r_value.pImpl = nullptr;
-        cout<<"moved consstructed."<<endl;
+        cout<<"moved constructed."<<endl;
     }
 
     //move assignment, saves heavylifting by transfering ownership and avoidign copy in case assingment
@@ -59,7 +59,7 @@ public:
     //destructor 
     ~BigBuffer(){
         delete[] pImpl;
-        cout<<this->buff_name<<" Destructed!"<<endl;
+        cout<<this->buff_name<<"Destructed!"<<endl;
     }
 };
 
@@ -81,12 +81,14 @@ int main(void){
                         //Unintentional copies of large objects are dangerous
                         //out copy constructor shall be efficient or better move to move semantics when ownership transffered 
                         //to avoid this penality
-    cout<<"------ while passing or returning by value ---------"<<endl;
+    cout<<"------ while passing  by value ---------"<<endl;
     func_A(b3);         //call by value, again copy, copy constructed
+    cout<<"------ while returning by value ---------"<<endl;
     b3 = func_B();      //Again return by value a penality ==> possible copy assignment unless RVO (C++17)
     
     cout<<"------ while adding to container ---------"<<endl;
     vector<BigBuffer> v;
+    v.reserve(10);      //this is very important , if we do not resever the size , then every time vector grow that many elecments are recreated
     v.push_back(b3);    //here one more copy constructor
 
     cout<<"------ move will make life better for ownership transfer ---------"<<endl;
@@ -94,6 +96,9 @@ int main(void){
     b4 = std::move(b2);           //and move assignment, they are by-default unless you implement for pIMPL things 
     cout<<"------ efficient container with move ---------"<<endl;
     v.push_back(std::move(b4)); 
+    cout<<"------ efficient container with emplace_back when constructing ---------"<<endl;
+    //v.emplace_back(std::move(b4)); //no benifit  
+    v.emplace_back(1000,"Emplaced_buff"); //temporary will be avoided , faster than creating a temporary and moving 
 
     return 0;
 }
